aa3011f6e259bd4b6e0f4c5ff8c3b7b6907cf9f8
KatieGott <Katie.Gott@target.com>
2022-07-15 15:29:04 -0500

    completed project

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"strings"
 )
 
 var originUnit string
@@ -17,12 +18,29 @@ var errInvalidArguments = errors.New("Invalid arguments")
 var errReadingInput = errors.New("Error reading input")
 
 func main() {
-
+	if len(os.Args) != 2 {
+		printError(errInvalidArguments)
+	}
+	originUnit = strings.ToUpper(os.Args[1])
 	for {
 		fmt.Print("What is the current temperature in " + originUnit + " ? ")
-
+		_, err := fmt.Scanln(&originValue)
+		if err != nil {
+			printError(errReadingInput)
+		}
+		if originUnit == "C" {
+			convertToFahrenheit(originValue)
+		} else {
+			convertToCelsius(originValue)
+		}
 		fmt.Print("Would you like to convert another temperature ? (y/n) ")
+		_, err = fmt.Scanln(&shouldConvertAgain)
 
+		if err != nil {
+			printError(errReadingInput)
+		}
+
+		shouldConvertAgain = strings.ToUpper(strings.TrimSpace(shouldConvertAgain))
 		if shouldConvertAgain != "Y" {
 			fmt.Println("Good bye!")
 			break
